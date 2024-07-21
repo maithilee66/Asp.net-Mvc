61784bef8579b45451d2f8121e51b320c713b21f
maithilee66 <110760285+maithilee66@users.noreply.github.com>
2024-07-22 02:18:45 +0530

    Update Employee.cs

--- DataAnnotations/Required/Models/Employee.cs
+++ DataAnnotations/Required/Models/Employee.cs
@@ -20,18 +20,4 @@ namespace iassess.Models
         public double? tax { get; set; }
     }
 }
-/*
 
-1. the required field message is bind with @Html.ValidationMessageFor(model => model.age) so we need to write both 
-2. veh message dikhata hain actually ValidationMessageFor
-2.add ? for nullabel types(such as int, double, or DateTime)
-4. we can also add custom message
-[Required(ErrorMessage = "name likh")]
-
-[Required] its default message =>The name field is required.
-5. @Html.ValidationMessageFor(model => model.age,"",new {@class="text-danger"})
-6.     if (!ModelState.IsValid)
-            {
-                return View("display");
-            }
-*/
